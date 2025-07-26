d32b72e7a7ab45efff88eefa9bea2acb9b6d7e88
anuragg1117 <anuragg117@yahoo.com>
2025-07-26 22:24:06 +0530

    commit four

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const heading = React.createElement("h1", {}, "Hello from Anurag");
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am Child")
+  )
+);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello from Anurag"
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
